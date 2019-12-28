cfa943facce75ca5db1fa89944cdf3fb6c9b0507
Shaw Innes <shaw@immortal.net.au>
2019-12-28 11:59:13 +1000

    Add Postgres Testing

--- handler.py
+++ handler.py
@@ -1,27 +1,33 @@
 import json
 import os
 import boto3
+import psycopg2
 from time import perf_counter as pc
-import socket
 
 
 class Config:
     """Lambda function runtime configuration"""
 
-    HOSTNAME = 'HOSTNAME'
-    PORT = 'PORT'
     TIMEOUT = 'TIMEOUT'
     REPORT_AS_CW_METRICS = 'REPORT_AS_CW_METRICS'
     CW_METRICS_NAMESPACE = 'CW_METRICS_NAMESPACE'
+    RDS_DATABASE = 'RDS_DATABASE'
+    RDS_HOSTNAME = 'RDS_HOSTNAME'
+    RDS_PORT = 'RDS_PORT'
+    RDS_USERNAME = 'RDS_USERNAME'
+    RDS_PASSWORD = 'RDS_PASSWORD'
 
     def __init__(self, event):
         self.event = event
         self.defaults = {
-            self.HOSTNAME: 'google.com.au',
-            self.PORT: 443,
             self.TIMEOUT: 120,
             self.REPORT_AS_CW_METRICS: '1',
             self.CW_METRICS_NAMESPACE: 'TcpPortCheck',
+            self.RDS_DATABASE: 'leases16',
+            self.RDS_HOSTNAME: 'localhost',
+            self.RDS_PORT: '5432',
+            self.RDS_USERNAME: 'leases16',
+            self.RDS_PASSWORD: 'leases16password'
         }
 
     def __get_property(self, property_name):
@@ -32,14 +38,6 @@ class Config:
         if property_name in self.defaults:
             return self.defaults[property_name]
         return None
-
-    @property
-    def hostname(self):
-        return self.__get_property(self.HOSTNAME)
-
-    @property
-    def port(self):
-        return self.__get_property(self.PORT)
 
     @property
     def timeout(self):
@@ -56,25 +54,48 @@ class Config:
             'namespace': self.__get_property(self.CW_METRICS_NAMESPACE),
         }
 
+    @property
+    def rds_database(self):
+        return self.__get_property(self.RDS_DATABASE)
 
-class PortCheck:
-    """Execution of HTTP(s) request"""
+    @property
+    def rds_hostname(self):
+        return self.__get_property(self.RDS_HOSTNAME)
+
+    @property
+    def rds_port(self):
+        return self.__get_property(self.RDS_PORT)
+
+    @property
+    def rds_username(self):
+        return self.__get_property(self.RDS_USERNAME)
+
+    @property
+    def rds_password(self):
+        return self.__get_property(self.RDS_PASSWORD)
+
+
+class PostgresCheck:
+    """Execution of Postgres request"""
 
     def __init__(self, config):
         self.config = config
 
     def execute(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(int(self.config.timeout))
         try:
             # start the stopwatch
             t0 = pc()
 
-            connect_result = sock.connect_ex((self.config.hostname, int(self.config.port)))
-            if connect_result == 0:
-                available = '1'
-            else:
-                available = '0'
+            db = psycopg2.connect(
+                database=self.config.rds_database,
+                user=self.config.rds_username,
+                password=self.config.rds_password,
+                host=self.config.rds_hostname,
+                port=self.config.rds_port,
+                connect_timeout=self.config.timeout
+            )
+
+            available = '1'
 
             # stop the stopwatch
             t1 = pc()
@@ -83,11 +104,11 @@ class PortCheck:
                 'TimeTaken': int((t1 - t0) * 1000),
                 'Available': available
             }
-            print(f"Socket connect result: {connect_result}")
+            print(f"Socket connect result: {db}")
             # return structure with data
             return result
         except Exception as e:
-            print(f"Failed to connect to {self.config.hostname}:{self.config.port}\n{e}")
+            print(f"Failed to connect to RDS {self.config.rds_database} {self.config.rds_hostname}:{self.config.rds_port}\n{e}")
             return {'Available': 0, 'Reason': str(e)}
 
 
@@ -101,7 +122,7 @@ class ResultReporter:
     def report(self, result):
         if self.options['enabled'] == '1':
             try:
-                endpoint = f"{self.config.hostname}:{self.config.port}"
+                endpoint = f"{self.config.rds_hostname}:{self.config.rds_port}"
                 cloudwatch = boto3.client('cloudwatch')
                 metric_data = [{
                     'MetricName': 'Available',
@@ -125,7 +146,7 @@ class ResultReporter:
                     MetricData=metric_data,
                     Namespace=self.config.cwoptions['namespace']
                 )
-                
+
                 print(f"Sent data to CloudWatch requestId=:{result['ResponseMetadata']['RequestId']}")
             except Exception as e:
                 print(f"Failed to publish metrics to CloudWatch:{e}")
@@ -135,16 +156,16 @@ def port_check(event, context):
     """Lambda function handler"""
 
     config = Config(event)
-    port_check = PortCheck(config)
 
-    result = port_check.execute()
+    postgres_check = PostgresCheck(config)
+    result = postgres_check.execute()
 
     # report results
     ResultReporter(config).report(result)
 
     result_json = json.dumps(result, indent=4)
     # log results
-    print(f"Result of checking  {config.hostname}:{config.port}\n{result_json}")
+    print(f"Result of checking {config.rds_database} {config.rds_hostname}:{config.rds_port}\n{result_json}")
 
     # return to caller
     return result
